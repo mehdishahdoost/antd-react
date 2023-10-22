a7a5eaaadf580bcd678a47418597b064de03f30d
mehdishahdoost <mhdshahdoost@gmail.com>
2023-10-22 12:45:49 +0200

    Antd Buttons POC

--- src/Button.js
+++ src/Button.js
@@ -5,11 +5,25 @@ import {PoweroffOutlined} from '@ant-design/icons';
 import {Button} from 'antd';
 
 export default function AntdButton() {
+
+  const [loading, setLoading] = useState(false);
+
+  const onClickFn = (e)=> {
+      setLoading(true);
+      setTimeout(()=> {
+        setLoading(false);
+      }, 2000);
+  }
+
   return (
       <div>
       <Button type='primary'>Click</Button>
       <Button type='primary' block>Click</Button>
       <Button type='link' href="https://google.com">Go to Google</Button>
+      <Button type='dashed'>Click</Button>
+      <Button type='text'>Click</Button>
+      <Button type='primary' block loading={loading} onClick={onClickFn}>Click</Button>
+      
 </div>
   );
 }
